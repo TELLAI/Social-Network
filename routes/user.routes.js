e421182f1c75e940f0e1261bc67efdd6657308b3
SIMPLON <tellai>
2021-04-17 02:37:31 +0200

    mis en place des routes login logout

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,8 +2,12 @@ const router = require("express").Router();
 const authController = require("../controllers/auth.controller")
 const userController = require("../controllers/user.cotroller")
 
+// auth
 router.post("/register", authController.signUp);
+router.post("/login", authController.signIn);
+router.get("/logout", authController.logOut);
 
+// user display : "block"
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.userInfo);
 router.put("/:id", userController.updateUser);
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/user.routes");
+const {checkUser} = require("./middleware/auth.middleware")
 require("./config/db");
 require("dotenv").config({ path: "./config/.env" });
 
 // bodyParser va nous permettre de mettre en forme les requtes 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
+// jwt 
+app.get('*', checkUser); // l'etoile nous permet de selectionner toutes les routes et ainsi de faire la verification sur chaque route
 
 // routes
 app.use("/api/user", userRoutes);
